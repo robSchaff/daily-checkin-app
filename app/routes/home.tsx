759b75250a7d1be4d014d98e2f068243870bec8f
robSchaff <rob.schaff@gmail.com>
2025-04-18 12:12:26 -0400

    Initial commit - hello words

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,24 @@
-import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+//import type { Route } from "./+types/home";
+//import { Welcome } from "../welcome/welcome";
+//
+//export function meta({}: Route.MetaArgs) {
+//  return [
+//    { title: "New React Router App" },
+//    { name: "description", content: "Welcome to React Router!" },
+//  ];//
+//}
 
-export function meta({}: Route.MetaArgs) {
-  return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
-  ];
-}
+//export default function Home() {
+//  return <Welcome />;
+//}
+
+
 
 export default function Home() {
-  return <Welcome />;
+  return (
+    <div>
+      <h1>Daily Check-In</h1>
+      <p>This is the start of something new.</p>
+    </div>
+  );
 }
